841b1a179986421a1140344918b426e155647de8
HamedSiasi <hamed@siasi.co.uk>
2016-07-19 13:42:56 +0100

    19/07/2016

---
+++ .build/SARA_NBIOT_EVK/GCC_ARM/blinky.d
@@ -0,0 +1,54 @@
+.build\SARA_NBIOT_EVK\GCC_ARM\.\blinky.o: blinky.cpp \
+ .build\SARA_NBIOT_EVK\GCC_ARM\mbed_config.h mbed-os/core/hal/api/mbed.h \
+ mbed-os/core/hal/api/toolchain.h mbed-os/core/hal/api/platform.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/TARGET_SARA_NBIOT/device.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/objects.h \
+ mbed-os/core/hal/targets/cmsis/TARGET_ublox/TARGET_HI2110/cmsis.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/HI2110_sdk/lib/arm_core/boudica/apps/boudica_apps.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/HI2110_sdk/lib/common_headers/neulfw.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/HI2110_sdk/lib/common_headers/commonc.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/HI2110_sdk/lib/common_headers/types.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/HI2110_sdk/lib/common_headers/config.h \
+ mbed-os/core/hal/targets/cmsis/core_cm0.h \
+ mbed-os/core/hal/targets/cmsis/core_cmInstr.h \
+ mbed-os/core/hal/targets/cmsis/core_cmFunc.h \
+ mbed-os/core/hal/targets/cmsis/TARGET_ublox/TARGET_HI2110/cmsis_nvic.h \
+ mbed-os/core/hal/targets/cmsis/TARGET_ublox/TARGET_HI2110/cmsis.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/PortNames.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/PeripheralNames.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/TARGET_SARA_NBIOT/PinNames.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/HI2110_sdk/lib/arm_core/boudica/shared/boudica_common_peripherals.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/gpio_object.h \
+ mbed-os/core/hal/api/mbed_assert.h \
+ mbed-os/core/hal/targets/hal/TARGET_ublox/TARGET_HI2110/PeripheralNames.h \
+ mbed-os/core/rtos/rtos/Mutex.h \
+ mbed-os/core/rtos/rtx/TARGET_CORTEX_M/cmsis_os.h \
+ mbed-os/core/hal/api/mbed_error.h mbed-os/core/hal/api/mbed_interface.h \
+ mbed-os/core/hal/api/mbed_assert.h mbed-os/core/hal/api/DigitalIn.h \
+ mbed-os/core/hal/hal/gpio_api.h mbed-os/core/hal/api/critical.h \
+ mbed-os/core/hal/api/DigitalOut.h mbed-os/core/hal/api/DigitalInOut.h \
+ mbed-os/core/hal/api/BusIn.h mbed-os/core/hal/api/BusOut.h \
+ mbed-os/core/hal/api/BusInOut.h mbed-os/core/hal/api/PortIn.h \
+ mbed-os/core/hal/hal/port_api.h mbed-os/core/hal/api/PortInOut.h \
+ mbed-os/core/hal/api/PortOut.h mbed-os/core/hal/api/AnalogIn.h \
+ mbed-os/core/hal/api/AnalogOut.h mbed-os/core/hal/api/PwmOut.h \
+ mbed-os/core/hal/api/Serial.h mbed-os/core/hal/api/Stream.h \
+ mbed-os/core/hal/api/FileLike.h mbed-os/core/hal/api/FileBase.h \
+ mbed-os/core/hal/api/FileHandle.h mbed-os/core/hal/api/SerialBase.h \
+ mbed-os/core/hal/api/Callback.h mbed-os/core/hal/hal/serial_api.h \
+ mbed-os/core/hal/hal/buffer.h mbed-os/core/hal/hal/dma_api.h \
+ mbed-os/core/hal/api/SPI.h mbed-os/core/hal/api/SPISlave.h \
+ mbed-os/core/hal/api/I2C.h mbed-os/core/hal/api/I2CSlave.h \
+ mbed-os/core/hal/api/Ethernet.h mbed-os/core/hal/api/CAN.h \
+ mbed-os/core/hal/api/RawSerial.h mbed-os/core/hal/api/Timer.h \
+ mbed-os/core/hal/hal/ticker_api.h mbed-os/core/hal/api/Ticker.h \
+ mbed-os/core/hal/api/TimerEvent.h mbed-os/core/hal/hal/us_ticker_api.h \
+ mbed-os/core/hal/hal/ticker_api.h mbed-os/core/hal/api/Timeout.h \
+ mbed-os/core/hal/api/LowPowerTimeout.h \
+ mbed-os/core/hal/hal/lp_ticker_api.h \
+ mbed-os/core/hal/api/LowPowerTicker.h \
+ mbed-os/core/hal/api/LowPowerTimer.h \
+ mbed-os/core/hal/api/LocalFileSystem.h \
+ mbed-os/core/hal/api/InterruptIn.h mbed-os/core/hal/hal/gpio_irq_api.h \
+ mbed-os/core/hal/api/wait_api.h mbed-os/core/hal/hal/sleep_api.h \
+ mbed-os/core/hal/api/rtc_time.h mbed-os/core/hal/api/FunctionPointer.h
---
+++ .build/SARA_NBIOT_EVK/GCC_ARM/mbed_config.h
@@ -0,0 +1,13 @@
+// Automatically generated configuration file.
+// DO NOT EDIT, content will be overwritten.
+
+#ifndef __MBED_CONFIG_DATA__
+#define __MBED_CONFIG_DATA__
+
+// Configuration parameters
+#define MBED_CONF_RTOS_PRESENT                1 // set by library:rtos
+#define MBED_CONF_CORE_STDIO_CONVERT_NEWLINES 0 // set by library:core
+// Macros
+#define UNITY_INCLUDE_CONFIG_H                  // defined by library:utest
+
+#endif
